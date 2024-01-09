280578380d680893cc34ceb2273deb26e1edc075
Mahesh Pawar <pawarmahesh723@gmail.com>
2024-01-09 12:44:20 +0530

    Intial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-green-600">
+      Namaste Everyone, Let's build Netflix GPT
+    </div>
+  );
+}
+
+export default App;
