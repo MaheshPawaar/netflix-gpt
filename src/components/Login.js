6f49ba7d4fa0c86af4dbfb578edd55311b8c73df
Mahesh Pawar <pawarmahesh723@gmail.com>
2024-01-10 12:06:26 +0530

    Created Sign In form

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,12 +5,22 @@ const Login = () => {
   return (
     <div>
       <Header />
-      <div>
+      <div className="absolute">
         <img
           src="https://assets.nflxext.com/ffe/siteui/vlv3/c38a2d52-138e-48a3-ab68-36787ece46b3/eeb03fc9-99c6-438e-824d-32917ce55783/IN-en-20240101-popsignuptwoweeks-perspective_alpha_website_large.jpg"
           alt="bg"
         />
       </div>
+      <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+        <h1 className="font-bold text-3xl py-4">Sign In</h1>
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-2 w-full bg-gray-700 rounded-lg"
+        />
+        <input type="text" placeholder="Password" className="p-4 my-2 w-full bg-gray-700 rounded-lg" />
+        <button className="p-4 my-2 bg-red-700 w-full rounded-lg">Sign In</button>
+      </form>
     </div>
   );
 };
